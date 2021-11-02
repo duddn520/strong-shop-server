8cb8dc197aa048cd9134ed45b813b26b48ebb000
duddn520 <duddn520@konkuk.ac.kr>
2021-11-02 20:50:22 +0900

    fix - response 수정

--- src/main/java/com/strongshop/mobile/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/strongshop/mobile/jwt/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.strongshop.mobile.domain.User.User;
 import com.strongshop.mobile.domain.User.UserRepository;
 import com.strongshop.mobile.dto.User.UserDto;
 import com.strongshop.mobile.exception.UserEmailNotFoundException;
+import com.strongshop.mobile.model.ApiResponse;
 import com.strongshop.mobile.service.TokenService;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -35,21 +36,13 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         String token = ((HttpServletRequest)request).getHeader("Auth");
-        PrintWriter writer = response.getWriter();
         response.setCharacterEncoding("UTF-8");
 
         if (token != null && jwtTokenProvider.verifyToken(token)) {     //토큰 유효성 검증 및 auth객체 생성 후 SecurityContextHolder에 등록.
             Authentication auth = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
-        else if(token == null)
-        {
-            writer.println("Token required");
-        }
-        else if(!jwtTokenProvider.verifyToken(token))
-        {
-            writer.println("Token expoired");
-        }
+
 
         chain.doFilter(request, response);
     }
