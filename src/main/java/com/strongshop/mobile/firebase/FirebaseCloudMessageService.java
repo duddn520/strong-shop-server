f6fd639212f9e6980cc11744c11f1bc868208f82
duddn520 <duddn520@konkuk.ac.kr>
2021-11-15 20:12:57 +0900

    add - CloudMessage를 위한 Firebase fcmMessage 추가 & 설정.

---
+++ src/main/java/com/strongshop/mobile/firebase/FirebaseCloudMessageService.java
@@ -0,0 +1,71 @@
+package com.strongshop.mobile.firebase;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FirebaseCloudMessageService {
+
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/strongshop-3fc81/messages:send";
+    private final ObjectMapper objectMapper;
+
+    private String getAccessToken() throws IOException{
+        String firebaseConfigPath = "/firebase/strongshop-3fc81-firebase-adminsdk-jypdg-8ae68bebe7.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+    public void sendMessageTo(String targetToken, String title, String body) throws  IOException {
+
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request)
+                .execute();
+
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException{
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        )
+                        .build()
+                )
+                .validate_only(false)
+                .build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+}
