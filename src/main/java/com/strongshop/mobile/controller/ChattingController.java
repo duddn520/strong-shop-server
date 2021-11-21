a83de68a35d76d9af6757ebd14ecea8d77828443
duddn520 <duddn520@konkuk.ac.kr>
2021-11-21 18:09:57 +0900

    add - chatcontroller -> Async

--- src/main/java/com/strongshop/mobile/controller/ChattingController.java
+++ src/main/java/com/strongshop/mobile/controller/ChattingController.java
@@ -8,8 +8,10 @@ import com.strongshop.mobile.model.ApiResponse;
 import com.strongshop.mobile.model.HttpResponseMsg;
 import com.strongshop.mobile.model.HttpStatusCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -23,22 +25,26 @@ public class ChattingController {
     private final FirebaseCloudMessageService firebaseCloudMessageService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @PutMapping("/api/chat")
-    public ResponseEntity<ApiResponse> chatAlarm(HttpServletRequest request)
-    {
-        Role role = Role.valueOf((String) jwtTokenProvider.getRole(jwtTokenProvider.getToken(request)));
+    @Async
+    public void chatAlarm(Role role) {
 
-        if(role.equals(Role.COMPANY))
-        {
+        if (role.equals(Role.COMPANY)) {
             try {
                 firebaseCloudMessageService.sendMessageTo("eVag6EG3sEFJpTbBaiwhO9:APA91bFTJhVTugHqwPZ9oyBbMSN6d0TAO_u293zB0Ge4MIJoZkCya72YTrGbKrTYaa2EEW3kTavVTq9apAYNWHLl-cYXUfc90K6IXlr8cmCUjbcHyfMT8fiKyiAfYXbacWuv4qTb7lkX"
                         , "title", "message", "1010101");
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 System.out.println("e.get = " + e.getMessage());
             }
         }
+    }
+
+
+    @PutMapping("/api/chat")
+    public ResponseEntity<ApiResponse> Alarm(HttpServletRequest request)
+    {
+        Role role = Role.valueOf((String) jwtTokenProvider.getRole(jwtTokenProvider.getToken(request)));
+
+        chatAlarm(role);
 
         return new ResponseEntity<>(ApiResponse.response(
                 HttpStatusCode.OK,
