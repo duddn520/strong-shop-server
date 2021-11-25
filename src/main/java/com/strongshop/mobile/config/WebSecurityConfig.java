91b61adea14e884454b50d679365de64f5eb659f
duddn520 <duddn520@konkuk.ac.kr>
2021-11-25 18:46:46 +0900

    fix - 오류 덮어두기.

--- src/main/java/com/strongshop/mobile/config/WebSecurityConfig.java
+++ src/main/java/com/strongshop/mobile/config/WebSecurityConfig.java
@@ -66,7 +66,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/login/**").permitAll()
                 .antMatchers("/token/**").permitAll()
-                .antMatchers("/api/contract/**").hasAnyRole("COMPANY","USER")
                 .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated();
 
