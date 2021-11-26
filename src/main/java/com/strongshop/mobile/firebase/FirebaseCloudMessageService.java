af0d6460e05e8c2b18adbbcc48b1718372f68dae
duddn520 <duddn520@konkuk.ac.kr>
2021-11-26 21:57:49 +0900

    add - 신규입찰 등록시 알림에 order_id 추가해서 유저에게 전달.(어떤 오더에 새 입찰이 들어왔는지 확인용.)

--- src/main/java/com/strongshop/mobile/firebase/FirebaseCloudMessageService.java
+++ src/main/java/com/strongshop/mobile/firebase/FirebaseCloudMessageService.java
@@ -62,10 +62,56 @@ public class FirebaseCloudMessageService {
             System.out.println(response.body().string());
     }
 
+    @Async
+    public void sendMessageTo(String targetToken, String title, String body, String index, Long orderId) throws IOException {
+        String message = makeMessage(targetToken, title, body, index,orderId);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request)
+                .execute();
+
+        System.out.println(response.body().string());
+    }
+
+
+
     private String makeMessage(String targetToken, String title, String body,String index) throws JsonProcessingException{
         Map<String,Object> map = new HashMap<>();
         map.put("index",index);
         map.put("time", LocalDateTime.now());
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .data(map)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        )
+                        .build()
+                )
+                .validate_only(false)
+                .build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String makeMessage(String targetToken, String title, String body, String index, Long orderId) throws JsonProcessingException
+    {
+        Map<String,Object> map = new HashMap<>();
+        map.put("index",index);
+        map.put("time", LocalDateTime.now());
+        map.put("order_id",orderId);
         FcmMessage fcmMessage = FcmMessage.builder()
                 .message(FcmMessage.Message.builder()
                         .token(targetToken)
