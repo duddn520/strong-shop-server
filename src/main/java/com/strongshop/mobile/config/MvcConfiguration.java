18fdd125df19db8090f2a88726b6b3272e45c04b
duddn520 <duddn520@konkuk.ac.kr>
2022-02-14 23:09:22 +0900

    fix - commonmultipartresolver -> standardservletmultipartresolver

--- src/main/java/com/strongshop/mobile/config/MvcConfiguration.java
+++ src/main/java/com/strongshop/mobile/config/MvcConfiguration.java
@@ -4,6 +4,7 @@ package com.strongshop.mobile.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,11 +12,8 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
     //파일 업로드 처리를 위한 빈 추가
     @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-
-        multipartResolver.setDefaultEncoding("UTF-8");
-        multipartResolver.setMaxUploadSize(256 * 1024 * 1024); //256MB
+    public StandardServletMultipartResolver multipartResolver() {
+        StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
 
         return multipartResolver;
     }
